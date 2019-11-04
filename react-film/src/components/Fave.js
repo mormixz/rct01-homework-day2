33d223004738830284fbb060751ec6bf5d3c8597
mormixz <Tuchchapol.m@gmail.com>
2019-11-04 11:13:32 +0700

    complete homework2

--- react-film/src/components/Fave.js
+++ react-film/src/components/Fave.js
@@ -16,7 +16,7 @@ class Fave extends Component {
         const isFave = this.state.isFave? 'remove_from_queue' : 'add_to_queue';
         return (
             <div className={`film-row-fave ${isFave}`}>
-                <p className="material-icons" onClick={this.handleClick}>add_to_queue</p>
+                <p className="material-icons" onClick={this.handleClick}>{isFave}</p>
             </div>
         );
     }
